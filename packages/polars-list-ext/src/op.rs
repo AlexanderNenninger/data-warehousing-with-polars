1cb7c84d35cdd874a35e22a7cbe8654e143d70b4
Alexander Nenninger <alexander.nenninger@live.de>
2026-07-06 15:43:23 +0200

    Correct attribution

--- packages/polars-list-ext/src/op.rs
+++ packages/polars-list-ext/src/op.rs
@@ -1,3 +1,6 @@
+// Ported verbatim from polars_list_utils (https://github.com/dashdeckers/polars_list_utils)
+// by Travis Hammond (dashdeckers).
+
 use crate::util::list_f64_dtype;
 use interp::{InterpMode, interp_slice};
 use polars::prelude::*;
